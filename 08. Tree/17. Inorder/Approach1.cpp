72bb2cda03cade8102a621b9f159e17ec38445ab
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-16 14:25:14 +0530

    Create Approach1.cpp

---
+++ 08. Tree/17. Inorder/Approach1.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fun(TreeNode* root, vector<int> &ans)
+    {
+        if(root==NULL)  return;
+
+        fun(root->left,ans);
+
+        ans.push_back(root->val);
+
+        fun(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        fun(root,ans);
+        return ans;
+    }
+};

6b5aaba86f983c1c64d494a09566b126ff903536
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-20 14:46:51 +0530

    Create Approach1.cpp

---
+++ 09. Graph/01. BFS/Approach1.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> ans;
+        queue<int> qe;
+        vector<int> vis(V,0);
+        qe.push(0);
+        vis[0] = 1;
+        
+        while(!qe.empty())
+        {
+            auto it = qe.front();
+            qe.pop();
+            
+            ans.push_back(it);
+            
+            for(auto child:adj[it])
+            {
+                if(!vis[child])
+                {
+                    vis[child] = 1;
+                    qe.push(child);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >>
+
+            E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

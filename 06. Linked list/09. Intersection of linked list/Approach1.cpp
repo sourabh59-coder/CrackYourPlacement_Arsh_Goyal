682265f21e431863bac67db0e50cc3ac9a0f5e86
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-21 17:52:54 +0530

    Create Approach1.cpp

---
+++ 06. Linked list/09. Intersection of linked list/Approach1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
+
+        while(ptr1 || ptr2)
+        {
+            if(ptr1==NULL)
+            {
+                ptr1= headB;
+            }
+            if(ptr2==NULL)
+            {
+                ptr2 = headA;
+            }
+
+            if(ptr1==ptr2)  return ptr1;
+
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+
+        return NULL;
+    }
+};

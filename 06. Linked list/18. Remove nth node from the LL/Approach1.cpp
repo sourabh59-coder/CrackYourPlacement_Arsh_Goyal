7e59e34eaee322c5a0a9564ea749dfe857103627
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-21 15:49:05 +0530

    Create Approach1.cpp

---
+++ 06. Linked list/18. Remove nth node from the LL/Approach1.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        head = dummy;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        for(int i=0;i<n;i++)    fast = fast->next;
+
+        while(fast->next!=NULL)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};

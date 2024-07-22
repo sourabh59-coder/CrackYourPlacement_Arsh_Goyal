2fc9c7c9fb4592a5142ff1facf4525fce3e9c013
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-22 13:16:25 +0530

    Create Approach1.cpp

---
+++ 09. Graph/24. Topological Sort/Approach1.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int> ans;
+	    vector<int> indegree(V,0);
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it: adj[i])
+	        {
+	            indegree[it]++;
+	        }
+	    }
+	    
+	    queue<int> qe;
+	    for(int i=0;i<V;i++)    if(indegree[i]==0)  qe.push(i);
+	    
+	    while(!qe.empty())
+	    {
+	        auto it = qe.front();
+	        qe.pop();
+	        
+	        ans.push_back(it);
+	        
+	        for(auto child:adj[it])
+	        {
+	            indegree[child]--;
+	            if(indegree[child]==0)  qe.push(child);
+	        }
+	    }
+	    
+	    return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends

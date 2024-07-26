80db583a067386922ace98a92c94db3c082d1c2c
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2024-07-26 13:12:21 +0530

    Update Approach1.cpp

--- 04. Maths/03. Max product/Approach1.cpp
+++ 04. Maths/03. Max product/Approach1.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maximumProduct(vector<int>& v) {
         long long ans = INT_MIN;
-        sort(v.begin(),v.end());
-        long long prod = 1;
+        sort(v.begin(), v.end());
         int n = v.size();
-        for(int i=0;i<n-2;i++)
-        {
-            prod = v[i] * v[i+1] * v[i+2];
-            ans = max(ans,prod);
-        }
+
+        long long val1 = (long long)v[0] * v[1] * v[n-1];
+        long long val2 = (long long)v[n-1] * v[n-2] * v[n-3];
+        ans = max(val1, val2);
+
         return (int)ans;
     }
 };
